0510690394a0c9aa60556419422cad958326e95d
Altai-man <altay-man@mail.ru>
2014-05-05 19:36:34 +0300

    Start to write views.

--- Layers/views.py
+++ Layers/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+# Django modules
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from django.views.generic import RedirectView, ListView, DetailView, View
+from django.conf import settings
+
+# Kasatou modules
+from Layers import models
+
