5621d6bbad138192e6cdd58906378f11245b3e3f
Altai-man <altay-man@mail.ru>
2014-05-12 18:06:38 +0300

    Modidy admin panel url into "bunny/", also fixed formatting.

--- Kasatou/urls.py
+++ Kasatou/urls.py
@@ -7,23 +7,18 @@ urlpatterns = patterns(
     '',
 
     # Admin
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^bunny/', include(admin.site.urls)),
 
-    # Index.
+    # Index
     url(r'^$', views.IndexView.as_view(), name='index'),
 
     # Board
-    url(r'^(?P<board_name>[a-z]{1,3})/(?P<page>[1-9]?)$',views.BoardView.as_view(),name='board_view'),
+    url(r'^(?P<board_name>[a-z]{1,3})/(?P<page>[1-9]?)$', views.BoardView.as_view(), name='board_view'),
 
     # Thread
-    url(r'^(?P<board_name>[a-z]{1,3})/thread/(?P<pk>[0-9]+)/$',views.ThreadView.as_view(),name='thread_view'),
+    url(r'^(?P<board_name>[a-z]{1,3})/thread/(?P<pk>[0-9]+)/$', views.ThreadView.as_view(), name='thread_view'),
 
-    url(r'^register/$', views.register, name='register'),
-
-    url(r'^login/$', views.user_login, name='login'),
-
-    url(r'^logout/$', views.user_logout, name='logout'),
-
+    # Search
     url(r'^search/$', views.search, name='search'),
 
     # Add thread
@@ -31,6 +26,13 @@ urlpatterns = patterns(
 
     # Add post
     url(r'^(?P<board_name>[a-z]{1,3})/thread/(?P<thread_id>[0-9]+)/add_post$',views.post_adding, name='post_add'),
+
+    # Urls for auth pages
+    url(r'^register/$', views.register, name='register'),
+
+    url(r'^login/$', views.user_login, name='login'),
+
+    url(r'^logout/$', views.user_logout, name='logout'),
 
    # Move to post
 #    url(r'^post/(?P<pk>[0-9]+)/$',views.PostView.as_view(),name='post_view'),
