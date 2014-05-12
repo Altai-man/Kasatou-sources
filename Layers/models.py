332e84a340976d970b66632f6de30bc43bf3a037
Altai-man <altay-man@mail.ru>
2014-05-12 19:35:56 +0300

    Add needed fields into postform(board_id and thread_id).

--- Layers/models.py
+++ Layers/models.py
@@ -244,7 +244,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['topic', 'text', 'image1', 'image2', 'image3', 'archive']
+        fields = ['topic', 'text', 'image1', 'image2', 'image3', 'archive', 'thread_id', 'board_id']
 
 
 # Signals
