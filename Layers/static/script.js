c26a3d99eb8871a1b46fa8032c7c59f6c8be726e
Altai-man <altay-man@mail.ru>
2014-05-20 13:46:26 +0300

    Update css files for update box style.

--- Layers/static/script.js
+++ Layers/static/script.js
@@ -1,3 +1,19 @@
+function temp_look(is_nsfw,selector) {
+
+    if (is_nsfw) {
+        selector.mouseenter(function() {
+            $(this).css('opacity','1.0');
+        });
+
+        selector.mouseleave(function() {
+            $(this).css('opacity','0');
+        });
+    } else
+        selector.unbind();
+
+}
+
+
 function show_linked(selector) {
     var original_selector = selector;
     selector.hover(
@@ -64,6 +80,130 @@ $(document).ready(function() {
         $(this).attr('src', $(this).attr('data-alt_name'));
         $(this).attr('data-alt_name', temp);
     });
+
+    // NSFW option and it's cookies
+    if ($.cookie('nsfw') == 'true') {
+        var selector = $('img.post_img');
+        selector.css('opacity','0');
+        $('#nsfw').attr('checked',true);
+        temp_look(true,selector);
+    }
+
+    $('#nsfw').change(function() {
+        var nsfw = $('#nsfw').is(':checked');
+        var selector = $('img.post_img');
+        if (nsfw) {
+            selector.css('opacity','0');
+            $.cookie('nsfw',true,{path: '/'});
+            temp_look(true,selector);
+        }
+        else {
+            selector.css('opacity','1.0');
+            $.cookie('nsfw',false,{path: '/'});
+            temp_look(false,selector);
+        }
+    });
+
+    // AJAX request for new posts in thread
+    $('#refresh').click(function() {
+        var csrftoken = $.cookie('csrftoken');
+        var thread_id = $('#thread_id').val();
+        var board_name =  $('#board_name').val();
+        var posts_numb = $('.post').length;
+        console.log(posts_numb);
+
+        $.ajax({
+            type:'GET',
+            crossDomain: false,
+            cache: false,
+            url: board_name+"thread/update/"+thread_id+'/'+posts_numb,
+            data: {},
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            },
+            success: function(output) {
+                if (output.is_new) {
+                    // Animated version
+                    var content = "<div class='inv_cont'>"+output.new_threads+"</div>"
+                    $('#post_cont').html($('#post_cont').html()+content);
+                    $('#post_cont div.inv_cont').last().fadeIn(700)
+
+                    // move to the page's end
+                    var url = location.href;
+                    location.href = "#bottom_cont";
+                    history.replaceState(null, null, url);
+                    
+                    // make NSFW new pics
+                    $('#nsfw').change()
+
+                    // Links
+                    window.links = $('div.link_to_content')
+                    window.links = show_linked(window.links)
+                }
+                else {
+                    $('#answer').html('Новых постов нет!');
+                    $('#answer').slideToggle("slow");
+                }
+            },
+        });
+    });
+
+
+    // send post via ajax
+    $('#send_post').click(function() {
+        var csrftoken = $.cookie('csrftoken');
+        var thread_id = $('#thread_id').val();
+        var board_name =  $('#board_name').val();
+        console.log(thread_id);
+        console.log(board_name);
+        var form = new FormData(document.getElementById('send_form'));
+        console.log(form);
+        $.ajax({
+            type:'POST',
+            crossdomain: false,
+            processData: false,
+            cache: false,
+            contentType: false,
+            MimeType: 'multipart/form-data',
+            url: board_name+"thread/"+thread_id+'/add_post/',
+            data: form,
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            },
+            success: function(output) {
+                if (output.success == true) {
+                    $('#refresh').click();
+                }
+                $('#form_table').html(output.form);
+            },
+        });
+    });
+
+
+    // Hide thread
+    // NSFW option and it's cookies
+    if ($.cookie('hide_thread') != '') {
+        var selector = $('img.post_img');
+        selector.css('opacity','0');
+        $('#nsfw').attr('checked',true);
+        temp_look(true,selector);
+    }
+
+    $('#nsfw').change(function() {
+        var nsfw = $('#nsfw').is(':checked');
+        var selector = $('img.post_img');
+        if (nsfw) {
+            selector.css('opacity','0');
+            $.cookie('nsfw',true,{path: '/'});
+            temp_look(true,selector);
+        }
+        else {
+            selector.css('opacity','1.0');
+            $.cookie('nsfw',false,{path: '/'});
+            temp_look(false,selector);
+        }
+    });
+
 
     // Show/hide options
     $('#options_button').click(function() {
